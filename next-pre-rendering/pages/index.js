922f51abde0cff46fe72727dfcdad26514aa06b1
Dheer Singh Rajpoot <Dheer.Singh@akqa.com>
2022-08-17 20:58:08 +0530

    updated links on homepage

--- next-pre-rendering/pages/index.js
+++ next-pre-rendering/pages/index.js
@@ -1,17 +1,46 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 function Home() {
   return (
     <>
       <h1>Next JS pre-rendering</h1>
-      <Link href='/users'>
-        <a>Users</a>
-      </Link>
-      <Link href='/posts'>
-        <a>Posts</a>
-      </Link>
+      <div>
+        <Link href="/users">
+          <a>Users</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/posts">
+          <a>Posts</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/events">
+          <a>Events</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/products">
+          <a>Products</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/news">
+          <a>News</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/dashboard">
+          <a>Dashboard</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/dashboard-swr">
+          <a>Dashboard-SWR</a>
+        </Link>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
